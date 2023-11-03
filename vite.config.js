d0334cc763e146998fcbc990d1ac2fb35d934163
arif123 <nashrullah2344@gmail.com>
2023-11-03 16:38:06 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-router-dom',
+  server: {
+    // Contoh: Menentukan port kustom
+    port: 3000,
+  },
+  build: {
+    // Contoh: Menentukan direktori output kustom
+    outDir: 'dist',
+  },
+  // Tambahkan konfigurasi tambahan sesuai kebutuhan Anda
 });
